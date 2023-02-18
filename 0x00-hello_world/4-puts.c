0451d24b1febfe1ffa4776e5ddc2debcfc88b379
Oliver Bassey <basseyoliver565@gmail.com>
2023-02-17 23:35:19 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a multilingual puzzle using the puts function
+ * return: 0 (success)
+*/
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
